ecae4c7c0c10f4bc60ff1ab116b69cf60cae26f5
BMESI <44075237+BMESI@users.noreply.github.com>
2019-08-25 20:21:54 -0500

    Forgot to add quotes for echo in latest update

--- testGit.php
+++ testGit.php
@@ -1,7 +1,7 @@
 <?php 
 echo "<h1> Ben Mesinovic </h1>";
 echo "<p>change from within Github...</p>";
-echo <h3>Definitions - copied from readme; updating remote from local...</h3>
+echo "<h3>Definitions - copied from readme; updating remote from local...</h3>
 <ul>        
 	<li>version control software - Software tool used to help control the updates of application developments.</li> 
         <li>add - Adds files to the staging area for copying to repository.</li> 
@@ -9,5 +9,5 @@ echo <h3>Definitions - copied from readme; updating remote from local...</h3>
         <li>push - Transmits changes to the master branch of repository.</li> 
         <li>pull - The pull command will take files from a repository and put them on the local working directory.</li> 
         <li>clone - This creates a copy of a local repository. </li> 
-</ul>
+</ul>";
 ?>
