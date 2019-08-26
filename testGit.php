28b4738173e744d3e5cf200cfb538d64b0196b26
BMESI <44075237+BMESI@users.noreply.github.com>
2019-08-25 20:18:51 -0500

    Adding changes from local using Gitbash

--- testGit.php
+++ testGit.php
@@ -1,4 +1,13 @@
 <?php 
 echo "<h1> Ben Mesinovic </h1>";
 echo "<p>change from within Github...</p>";
+echo <h3>Definitions - copied from readme; updating remote from local...</h3>
+<ul>        
+	<li>version control software - Software tool used to help control the updates of application developments.</li> 
+        <li>add - Adds files to the staging area for copying to repository.</li> 
+        <li>commit - Commits any changes done to the files that have been added to stage.</li> 
+        <li>push - Transmits changes to the master branch of repository.</li> 
+        <li>pull - The pull command will take files from a repository and put them on the local working directory.</li> 
+        <li>clone - This creates a copy of a local repository. </li> 
+</ul>
 ?>
